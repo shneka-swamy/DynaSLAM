07009dffa153bcf767e8b0d3f9ffbe11006b85cd
shneka-swamy <shneka.swamy@gmail.com>
2023-06-08 11:13:52 -0600

    Chnages that compile the code

--- build.sh
+++ build.sh
@@ -1,18 +1,22 @@
 echo "Configuring and building Thirdparty/DBoW2 ..."
 
 cd Thirdparty/DBoW2
-mkdir build
+mkdir -p build
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release
+mkdir -p conan
+pushd conan
+conan install ../.. --build=missing --settings build_type=Release
+popd
+cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_MODULE_PATH=$PWD/conan -DCMAKE_PREFIX_PATH=$PWD/conan -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
 make -j
 
 cd ../../g2o
 
 echo "Configuring and building Thirdparty/g2o ..."
 
-mkdir build
+mkdir -p build
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release
+cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
 make -j
 
 cd ../../../
@@ -25,7 +29,13 @@ cd ..
 
 echo "Configuring and building DynaSLAM ..."
 
-mkdir build
+mkdir -p build
 cd build
+mkdir -p conan
+pushd conan
+conan install ../.. --build=missing --settings build_type=Debug
+conan install ../.. --build=missing --settings build_type=Release
+popd
 cmake .. -DCMAKE_BUILD_TYPE=Release
-make -j
+cmake --build .
+cd ..
